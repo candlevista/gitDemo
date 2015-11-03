94fdc68e2d962b1fa61c6f0ea8560970832b046b
candlevista <864994121@qq.com>
2015-11-03 17:06:08 +0800

    add cell

---
+++ aaaaa/aaaaa/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  aaaaa
+//
+//  Created by Ibokan2 on 15/11/3.
+//  Copyright (c) 2015年 Ibokan2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ aaaaa/aaaaa/TableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.m
+//  aaaaa
+//
+//  Created by Ibokan2 on 15/11/3.
+//  Copyright (c) 2015年 Ibokan2. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
